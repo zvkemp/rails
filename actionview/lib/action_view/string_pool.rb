45eaeda56458c3ec91914f2cd5d4b7a6754ea79b
Xavier Noria <fxn@hashref.com>
2013-12-13 16:02:07 +0100

    documents a policy for string pools, and marks them private

--- actionview/lib/action_view/string_pool.rb
+++ actionview/lib/action_view/string_pool.rb
@@ -1,5 +1,10 @@
 module ActionView
-  module StringPool
+  # This module has constants for string literals whose extraction has proven to
+  # be worthwhile. This set should be minimal, new constants can only be added
+  # if proved to make a difference in benchmarks using production applications.
+  #
+  # String pools are private and they can change anytime.
+  module StringPool # :nodoc:
     EMPTY      = ''.freeze
     NEWLINE    = "\n".freeze
     SLASH      = '/'.freeze
